c76a3b1bc4ea4123bf024069cc75d6f3e4e1a836
360tetsu360 <tetsuhiko.game.minecraft@gmail.com>
2021-11-24 20:46:52 +0900

    .

--- src/connection.rs
+++ src/connection.rs
@@ -1,15 +1,37 @@
-use std::{
-    net::{SocketAddr, UdpSocket},
-    rc::Rc,
-};
+use std::{net::{SocketAddr}, sync::Arc,time::Instant};
+use tokio::net::UdpSocket;
+use crate::raknet::RaknetEvent;
+
 pub struct Connection {
-    pub address: SocketAddr,
-    pub socket: Rc<UdpSocket>,
-    //send_query : Vec<Frame>,
+    pub address : SocketAddr,
+    _socket : Arc<UdpSocket>,
+    pub event_queue : Vec<RaknetEvent>,
+    pub guid : u64,
+    timer : Instant,
+    last_recieve : u128
 }
 
 impl Connection {
-    //pub fn new(addr : SocketAddr){}
-    //pub fn handle_datagram(&self, buf: &mut [u8]) {}
-    //pub fn send(&mut self,packet : Frame) {}
+    pub fn new(address: SocketAddr,socket : Arc<UdpSocket>,guid : u64,timer : Instant) -> Self{
+        Self{
+            address,
+            _socket : socket,
+            event_queue : vec![RaknetEvent::Connected(address,guid)],
+            guid,
+            timer,
+            last_recieve : timer.elapsed().as_millis()
+        }
+    }
+    pub fn update(&mut self) {
+        self.event_queue.clear();
+        let time = self.timer.elapsed().as_millis();
+        if (time - self.last_recieve) > 10000 {
+            self.disconnect();
+        } 
+    }
+    pub fn handle_datagram(&self,_buff :&[u8]) {
+    }
+    pub fn disconnect(&mut self) {
+        self.event_queue.push(RaknetEvent::Disconnected(self.address,self.guid));
+    }
 }
--- src/raknet.rs
+++ src/raknet.rs
@@ -1,6 +1,6 @@
 use rand::random;
-use std::{collections::HashMap, net::SocketAddr};
-use tokio::net::{ToSocketAddrs, UdpSocket};
+use std::{collections::HashMap, io::{Error, Result}, net::SocketAddr, sync::Arc,time::Instant};
+use tokio::{net::{ToSocketAddrs, UdpSocket}, sync::Mutex};
 
 use crate::{
     connection::Connection,
@@ -8,73 +8,113 @@ use crate::{
         open_connection_reply1::OpenConnectionReply1, open_connection_reply2::OpenConnectionReply2,
         unconnected_pong::UnconnectedPong, Packets,
     },
+    packet::Packet
 };
+
+pub enum RaknetEvent {
+    Packet(Packet),
+    Connected(SocketAddr,u64),
+    Disconnected(SocketAddr,u64),
+    Error(SocketAddr,Error)
+}
+
+impl Clone for RaknetEvent {
+    fn clone(&self) -> Self {
+        match &*self {
+            RaknetEvent::Packet(p) => RaknetEvent::Packet(p.clone()),
+            RaknetEvent::Connected(p,e) => RaknetEvent::Connected(*p,*e),
+            RaknetEvent::Disconnected(p,e) => RaknetEvent::Disconnected(*p,*e),
+            RaknetEvent::Error(p,err) => RaknetEvent::Error(*p,std::io::Error::new(err.kind(), err.to_string()))
+        }
+    }
+}
+
 pub struct Server {
-    pub socket: UdpSocket,
-    pub connection: HashMap<SocketAddr, Connection>,
+    pub socket: Arc<UdpSocket>,
+    pub connection: Arc<Mutex<HashMap<SocketAddr, Connection>>>,
     pub id: u64,
-    pub title: String,
+    pub title: Arc<String>,
+    time : Instant,
 }
 
 impl Server {
     pub async fn new(address: impl ToSocketAddrs, title: String) -> Self {
         println!("packet!");
         Self {
-            socket: UdpSocket::bind(address).await.unwrap(),
-            connection: HashMap::new(),
+            socket: Arc::new(UdpSocket::bind(address).await.unwrap()),
+            connection: Arc::new(Mutex::new(HashMap::new())),
             id: random::<u64>(),
-            title,
+            title : Arc::new(title),
+            time : Instant::now(),
         }
     }
 
     pub async fn listen(&self) {
-        loop {
-            let mut v = [0; 2048];
-            let (_size, source) = self
-                .socket
-                .recv_from(&mut v)
-                .await
-                .expect("Failed to receive datagram");
-            self.handle(v, source).await;
-            // これsourceに元アドレス入る
-        }
+        let socket2 = self.socket.clone();
+        let connections2 = self.connection.clone();
+        let id = self.id;
+        let motd = self.title.clone();
+        let time = self.time;
+        tokio::spawn(async move {
+            let mut v = [0u8;1500];
+            loop {
+                let (_size,source) = socket2.recv_from(&mut v).await.unwrap();
+                if !connections2.lock().await.contains_key(&source) {
+                    //not connected
+                    let packet = match Packets::decode(&mut v) {
+                        Ok(s) => s,
+                        Err(err) => {
+                            println!("{}", &err);
+                            Packets::Error(())
+                        }
+                    };
+                    match packet {
+                        Packets::UnconnectedPing(p) => {
+                            let pong = UnconnectedPong::new(p.time,id, motd.to_string());
+                            if let Ok(data) = Packets::UnconnectedPong(pong).encode() {
+                                let _ = socket2.send_to(&data, source).await.unwrap();
+                            };
+                        }
+                        Packets::OpenConnectionRequest1(p) => {
+                            let ocreply1 = OpenConnectionReply1::new(id, false, p.mtu_size);
+                            if let Ok(data) = Packets::OpenConnectionReply1(ocreply1).encode() {
+                                let _ = socket2.send_to(&data, source).await.unwrap();
+                            };
+                        }
+                        Packets::OpenConnectionRequest2(p) => {
+                            let ocreply2 = OpenConnectionReply2::new(id, source, p.mtu, false);
+                            if let Ok(data) = Packets::OpenConnectionReply2(ocreply2).encode() {
+                                let _ = socket2.send_to(&data, source).await.unwrap();
+                                connections2.lock().await.insert(source, Connection::new(source,socket2.clone(),id,time));
+                            };
+                            //connected!
+                        }
+                        _ => {}
+                    }
+                } else {
+                    connections2.lock().await[&source].handle_datagram(v.as_mut());
+                }
+            }
+        });
     }
 
-    pub async fn handle<T: AsMut<[u8]>>(&self, mut buff: T, source: SocketAddr) {
-        if !self.connection.contains_key(&source) {
-            //not connected
-            let packet = match Packets::decode(buff.as_mut()) {
-                Ok(s) => s,
-                Err(err) => {
-                    println!("{}", &err);
-                    Packets::Error(())
+    pub async fn recv(&self) -> Result<Vec<RaknetEvent>> {
+        let mut events : Vec<RaknetEvent> = vec![];
+        let mut disconnected_clients = vec![];
+        for (_address,connection) in self.connection.lock().await.iter_mut() {
+            for event in connection.event_queue.clone() {
+                if let RaknetEvent::Disconnected(addr,_guid) = event {
+                    disconnected_clients.push(addr);
                 }
-            };
-            match packet {
-                Packets::UnconnectedPing(p) => {
-                    let pong = UnconnectedPong::new(p.time, self.id, self.title.clone());
-                    if let Ok(data) = Packets::UnconnectedPong(pong).encode() {
-                        let _ = self.socket.send_to(&data, source).await.unwrap();
-                    };
-                }
-                Packets::OpenConnectionRequest1(p) => {
-                    let ocreply1 = OpenConnectionReply1::new(self.id, false, p.mtu_size);
-                    if let Ok(data) = Packets::OpenConnectionReply1(ocreply1).encode() {
-                        let _ = self.socket.send_to(&data, source).await.unwrap();
-                    };
-                }
-                Packets::OpenConnectionRequest2(p) => {
-                    let ocreply2 = OpenConnectionReply2::new(self.id, source, p.mtu, false);
-                    if let Ok(data) = Packets::OpenConnectionReply2(ocreply2).encode() {
-                        let _ = self.socket.send_to(&data, source).await.unwrap();
-                    };
-                    //connected!
-                }
-                _ => {}
+                events.push(event);
             }
-        } else {
-            //self.connection[&source].handle_datagram(buff.as_mut());
+            connection.update();
         }
+        for addr in disconnected_clients.iter() {
+            self.connection.lock().await.remove(addr);
+        }
+        disconnected_clients.clear();
+        Ok(events)
     }
 }
 
