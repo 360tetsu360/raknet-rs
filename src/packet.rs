d84c4c3e8c9804a5944827e125785d5a70fb5005
360tetsu360 <tetsuhiko.game.minecraft@gmail.com>
2021-11-24 20:48:46 +0900

    .

--- src/packet.rs
+++ src/packet.rs
@@ -12,8 +12,8 @@ pub struct FrameSet {
 
 #[derive(Clone)]
 pub struct Packet {
-    pub address : SocketAddr,
-    pub guid : u64,
-    pub length : usize,
-    pub data : Vec<u8>
+    pub address: SocketAddr,
+    pub guid: u64,
+    pub length: usize,
+    pub data: Vec<u8>,
 }
