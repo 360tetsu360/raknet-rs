36a0caac09fbb123fed0a3979dd6f3f20adcb678
360tetsu360 <tetsuhiko.game.minecraft@gmail.com>
2021-12-17 15:22:19 +0900

    split packet

---
+++ examples/bad_guy.rs
@@ -0,0 +1,49 @@
+use raknet::raknet::{Client, RaknetEvent};
+use std::net::ToSocketAddrs;
+
+async fn client() {
+    let mut remote = "127.0.0.1:19132".to_socket_addrs().unwrap();
+    //let remote: SocketAddr = "hivebedrock.network".parse().expect("could not parse addr");
+    let mut client = Client::new(remote.next().unwrap(), true);
+    client.listen().await;
+    client.connect().await;
+    loop {
+        tokio::time::sleep(std::time::Duration::from_millis(50)).await;
+        let events = client.recv().await.unwrap();
+        let mut disconnected = false;
+        for event in events {
+            match event {
+                RaknetEvent::Connected(addr, guid) => {
+                    println!("connected {} {}", addr, &guid);
+                    client.send(&[0xffu8; 4096]).await.unwrap();
+                }
+                RaknetEvent::Disconnected(addr, guid, _reason) => {
+                    println!("disconnected {} {}", addr, &guid);
+                    disconnected = true;
+                    break;
+                }
+                RaknetEvent::Packet(_packet) => {
+                    client.disconnect().await;
+                }
+                _ => {}
+            }
+        }
+        if disconnected {
+            break;
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    for _ in 0..10 {
+        tokio::spawn(async move {
+            loop {
+                client().await;
+            }
+        });
+    }
+    loop {
+        client().await;
+    }
+}
